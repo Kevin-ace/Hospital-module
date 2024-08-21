31a5643260708d0c91f5d1644cc98cf1979d9251
Kevin-ace <kevinkipruto83@gmail.com>
2024-08-21 11:40:54 +0300

    Create an init file and a manifest file

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Hospital management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'Kevin',
+    'sequence': -100,
+    'summary': 'Hospital management system',
+    'description': """hospital management system""",
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+        'views/female_patient_view.xml',
+        'views/male_patient_view.xml'
+    ],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
